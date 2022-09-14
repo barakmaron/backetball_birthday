1d0345ea0106d9a6759617ab9f11e02f137c012a
Barak Maron <Barak062@gmail.com>
2022-09-14 15:50:10 +0300

    feat: adds Constants file

---
+++ server/Constants.js
@@ -0,0 +1,13 @@
+const DateStampFormat = "YYYY-MM-DD";
+
+const USER_LOGIN_ERROR = "Email And password do not match";
+
+const REFRESH_TOKEN_MAX_AGE = 24 * 60 * 60 * 1000;
+
+const Constants = {
+    DateStampFormat,
+    USER_LOGIN_ERROR,
+    REFRESH_TOKEN_MAX_AGE
+};
+
+export default Constants;
