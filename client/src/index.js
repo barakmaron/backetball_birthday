97d48e8a67d464d4b2b8add7244aabb7d3fa7ade
Barak Maron <Barak062@gmail.com>
2022-09-16 17:06:30 +0300

    refactor: changes App component to App connector

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import AppConnector from './AppConnector';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
@@ -10,9 +10,9 @@ import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
+  <Provider store={store}>    
     <BrowserRouter>
-      <App />
+      <AppConnector></AppConnector>
     </BrowserRouter>
   </Provider>
 );
