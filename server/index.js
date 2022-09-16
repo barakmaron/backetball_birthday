6cf58395c125abf36c43203f52b023dd1b84c5b0
Barak Maron <Barak062@gmail.com>
2022-09-16 17:05:45 +0300

    feat: adds sequilze orm to server

--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import routes from './routes/index.js';
+import { sequelize } from "./db/models/index.js";
 import dotenv from 'dotenv';
 dotenv.config();
+
+Promise.resolve(sequelize.sync());
 
 const port = process.env.PORT;
 const app = express();
 
 app.use([morgan("common"), cors(), express.json()]);
+
+app.use('/', routes);
 
 app.get("/", (req, res) => {
     return res.status(200).json("ok");
