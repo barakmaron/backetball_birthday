b8ed4beaf74f2dbf8a5031cbcf5c70c88188d2c7
Barak Maron <Barak062@gmail.com>
2022-09-16 17:02:19 +0300

    feat: adds toast for error and successful messages

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,55 @@
+import React, { useCallback } from "react";
 import { Route, Routes } from "react-router-dom";
+import Footer from "./Components/Footer/Footer";
+import NavBar from "./Components/NavBar/NavBar";
 import StickyWhatsapp from "./Components/StickyWhatsapp";
 import Home from "./Pages/Home/Home";
+import CalendarPageConnector from './Pages/Calendar/CalendarConnector';
+import Constants from "./Constants";
+import UploadImageConnector from './Components/UploadImage/UploadImageConnector';
+import AdminConnector from "./Pages/Admin/AdminConnector";
+import { Toast } from 'monday-ui-react-core';
 
-function App() {
-  return (<>    
+function App({
+  failed,
+  successful,
+  message,
+  InitErrorSuccessAction
+}) {
+
+  const close_toast = useCallback(() => {
+		InitErrorSuccessAction();
+	}, [InitErrorSuccessAction]);
+
+  return (<>   
+    <header>
+      <NavBar
+      routes={Constants.routes}></NavBar>  
+    </header> 
     <Routes>
       <Route path="/" element={<Home></Home>} />
+      <Route path="/calendar" element={<CalendarPageConnector></CalendarPageConnector>} />
+      <Route path="/admin" element={<AdminConnector></AdminConnector>}>
+        <Route path="upload" element={<UploadImageConnector></UploadImageConnector>}/>
+        <Route path="calendar" element={<CalendarPageConnector></CalendarPageConnector>}/>
+      </Route>
     </Routes>
-    <footer className="h-44">
-        
+    <footer className=" bg-orange-500">
+        <Footer></Footer>
     </footer>
-    <StickyWhatsapp></StickyWhatsapp>   
+    <StickyWhatsapp></StickyWhatsapp>  
+    <Toast 
+    open={successful} 
+    type={Toast.types.POSITIVE} 
+    autoHideDuration={3000} 
+    onClose={close_toast}
+    style={{"z-index": 999}}>{message}</Toast> 
+    <Toast 
+    open={failed} 
+    type={Toast.types.NEGATIVE} 
+    autoHideDuration={3000}
+    onClose={close_toast}
+    style={{"z-index": 999}}>{message}</Toast> 
  </>);
 }
 
