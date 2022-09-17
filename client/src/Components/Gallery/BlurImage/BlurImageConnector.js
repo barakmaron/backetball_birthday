002bcb1e6aa7cfa378e98a550cf7efe49b80d516
Barak Maron <Barak062@gmail.com>
2022-09-17 14:48:07 +0300

    feat: adds show more images button

--- client/src/Components/Gallery/BlurImage/BlurImageConnector.js
+++ client/src/Components/Gallery/BlurImage/BlurImageConnector.js
@@ -2,9 +2,11 @@ import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import BlurImage from "./BlurImage";
 import { DeleteImageAction, EditAltAction } from "../../../redux/actions/ImageActions";
+import { getLoggedIn } from '../../../redux/selectors/UserSelector';
 
 const mapStateToProps = (state, ownProps) => {
-    return { ...ownProps };
+    const logged_in = getLoggedIn(state);
+    return { ...ownProps, logged_in };
 };
 
 const mapActionsToProps = dispatch => {
--- client/src/Components/Gallery/Gallery.jsx
+++ client/src/Components/Gallery/Gallery.jsx
@@ -1,4 +1,6 @@
-import React, { useEffect } from 'react';
+import { Button } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import Constants from '../../Constants';
 import BlurImageConnector from './BlurImage/BlurImageConnector';
 
 
@@ -6,18 +8,26 @@ const Gallery = ({
   images, 
   GetImagesAction
  }) => {
+  
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     GetImagesAction();
   }, []);
 
-  return (
+  return (<>
     <div className="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
         <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-            {images?.map(image => <BlurImageConnector upload_page={true} key={`image-${image.id}`} image={image}></BlurImageConnector>)}
-        </div>
+            {images?.filter((image, index) => index < page * Constants.number_images_per_page).map(image => <BlurImageConnector key={`image-${image.id}`} image={image}></BlurImageConnector>)}
+        </div>        
     </div>
-  )
+    <div className='flex w-fit mx-auto my-4'>
+    { page * Constants.number_images_per_page < images?.length && 
+          <Button 
+          onClick={() => setPage(page => page + 1)} 
+          variant="contained">הצג עוד תמונות</Button> }
+    </div>
+  </>);
 }
 
 export default Gallery;
