e848950199ce00b72ee706afd8df28c9d1c126f2
BarakMaron <barak062@gmail.com>
2022-10-02 15:09:25 +0300

    feat: adds send mail on calendar event

--- server/services/MailService.js
+++ server/services/MailService.js
@@ -8,14 +8,15 @@ const transporter = nodemailer.createTransport({
     }
 });
 
-export const mailOption = (from, full_name, phone_number) => ({
+export const mailOption = (from, full_name, phone_number, date) => ({
     from,
     to: process.env.EMAIL_USER_NAME,
-    subject: "New contact form!!",
-    text: `name: ${full_name}\nphone: ${phone_number}\nemail: ${from}`
+    subject: date ? "New Calendar event!!" : "New contact form!!",
+    text: `name: ${full_name}\nphone: ${phone_number}\nemail: ${from}\ndate:${date || new Date()}`
 });
 
 const SendMail = (mailOption) => transporter.sendMail(mailOption, (error, info) => {
+    info && console.log("Email was sent successfully");
     if(error)
         throw error;
 });
