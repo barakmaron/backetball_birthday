2f44431afb37434a762fe477448e2cf893e8d043
BarakMaron <barak062@gmail.com>
2022-10-03 09:06:42 +0300

    feat: adds sha512 to password before sending api requset

--- client/src/redux/actions/UserActions.js
+++ client/src/redux/actions/UserActions.js
@@ -2,6 +2,7 @@ import SendApiRequest from "../../services/ApiService";
 import ACTIONS from "./actionsConstants/UserActionsConstants";
 import Constants from "../../Constants";
 import { failedAction } from "./ViewActions";
+import UserService from "../../services/UserService";
 
 const Login = () => ({
     type: ACTIONS.LOGIN
@@ -14,7 +15,8 @@ const Logout = () => ({
 export const LoginAction = (login_form) => {
     return async (dispatch) => {
         try {
-            await SendApiRequest('auth/login', Constants.API_METHODS.POST, login_form);
+            const form_encrypted_password = await UserService.EncryptForm(login_form);
+            await SendApiRequest('auth/login', Constants.API_METHODS.POST, form_encrypted_password);
             dispatch(Login());
         } catch (err) {
             dispatch(failedAction(err.message));
