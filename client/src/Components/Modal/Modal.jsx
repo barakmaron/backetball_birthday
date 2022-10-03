cb4743c87b15f4120ad177969c5c904e8b0813b9
BarakMaron <barak062@gmail.com>
2022-10-03 09:05:10 +0300

    feat: adds class name to modal children

--- client/src/Components/Modal/Modal.jsx
+++ client/src/Components/Modal/Modal.jsx
@@ -2,18 +2,24 @@ import React from 'react';
 import style from './Modal.module.css';
 import { FaTimes } from 'react-icons/fa';
 
-export default function Modal({ setClose, children }) {
+export default function Modal({ 
+    setClose, 
+    children, 
+    className 
+}) {
     return(
         <div className={style.modal_wrapper} onClick={() => setClose()}>
             <div className={style.modal_inner} onClick={e => e.stopPropagation()}>
                 <div className={style.close_button} onClick={() => setClose()}>
                     <FaTimes></FaTimes>
                 </div>
-                {React.Children?.map(children, child => {
-                    if(React.isValidElement(child))
-                        return React.cloneElement(child);
-                    return child;
-                })}
+                <div className={className}>
+                    {React.Children?.map(children, child => {
+                        if(React.isValidElement(child))
+                            return React.cloneElement(child);
+                        return child;
+                    })}
+                </div>
             </div>
         </div>
     )
