a320d7aa4f92a36912835b81b61ba248ee649323
BarakMaron <barak062@gmail.com>
2022-10-07 14:55:34 +0300

    refactor: home page style change

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -3,23 +3,20 @@ import { BiTimeFive } from 'react-icons/bi';
 import { FaChild } from 'react-icons/fa';
 import { GiTennisCourt } from 'react-icons/gi';
 import GalleryConnector from '../../Components/Gallery/GalleryConnector';
+import RecommendationGalleryConnector from '../../Components/RecommendationGallery/RecommendationGalleryConnector';
 import styles from './Home.module.css';
 
 const Home = () => {
  
   return (
-    <div className={styles.container}>
-        <div className={`${styles.main_container} grid grid-cols-1 py-16 gap-y-10  gap-x-6 lg:grid-cols-4 xl:grid-cols-4 xl:gap-x-16`}>         
-          <div></div>
-          <div className={styles.main_image}>
-            <div className={styles.conpety}></div>
-            <div className={styles.basketball}></div>
-            <div className={styles.trophy}></div>  
-          </div> 
-          <div className='justify-center flex items-center'>
-            <h1 className={` sm:text-4xl text-center text-2xl font-bold text-orange-600`}>כדורסלדת</h1>
+    <div className={styles.container}>        
+      <div className={`${styles.main_container} flex justify-center bg-position-x bg-position-y xl:bg-position-x-lg xl:bg-position-y-lg py-10`}>
+          <div className='justify-center flex items-center flex-col gap-5'>
+            <h1 className={` sm:text-9xl text-center text-2xl font-bold text-orange-600`}>כדורסלדת</h1>
+            <h2 className='text-orange-600 sm:text-6xl font-bold'>יום הולדת כדורסל</h2>
           </div>         
         </div>
+        <GalleryConnector></GalleryConnector>
         <div className={`${styles.second_container} mx-auto py-4 px-16 sm:py-5 sm:px-24`}>
           <div className={`grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-8 mx-auto`}>
             <div className={styles.card} dir="rtl">
@@ -32,32 +29,37 @@ const Home = () => {
             </div>
             <div className={styles.card} dir="rtl">
                 <span>
-                  <BiTimeFive></BiTimeFive>
+                  <FaChild></FaChild>                  
                 </span>
-                <h2>פעילות</h2>
+                <h2>חוייה</h2>
                 <p>
-                  הפעילות מחולקת למספר חלקים.<br></br>
-                  משחקונים מאתגרים וחווייתיים שמטרתם חימום  ו"פריקת אנרגיה" לילדים.<br></br>
-                  תחנות שונות ומגוונות אותם מעבירים מדריכי הפעילות. כגון: סללומים, זריקות לסל, תחרות קליעות מעמדות שונות במגרש תוך מחשבה לאפשר לכל ילד/ה להנות בפעילות המיוחדת גם אם לא קשורים לענף הכדורסל.<br></br>
-                  משחקי כדורסל להעצמת ילד/ת היום הולדת, הם בוחרים מובילים הם הקפטנים ומחלקי התשורות.
+                  זמן הפעילות הוא כשעה וחצי, שעתיים כולל טקס הדלקת נרות.
+                  המדריכים מגיעים עם כל הציוד הנדרש לפעילות.<br></br>
+                  מטרת הפעילות היא להעצים ולשמח את ילד היום הולדת 😁<br></br>
+                  יחולקו פרסים והפתעות לכל משתתפי יום ההולדת ולילד היום הולדת בפרט🎖<br></br>
+                  לפעילות מגיע מדריך כדורסל מוסמך.
+                  ניתן להתאים את הפעילות לכל רמה.
                 </p>
             </div>            
             <div className={styles.card} dir="rtl">
               <span>
-                <FaChild></FaChild>
+                <BiTimeFive></BiTimeFive>
               </span>
-              <h2>חוייה</h2>
-              <p>
-                זמן הפעילות הוא כשעה וחצי <br></br>(שעתיים עם טקס הדלקת הנרות).<br></br>
-                המדריכים מגיעים עם כל הציוד הנדרש לפעילות.<br></br>
-                מטרת הפעילות היא להעצים ולשמח את ילד היום הולדת!<br></br>
-                במהלך הפעילות ובסופה ישנם פרסים ומתנות לכל המשתתפים ולילד יום ההולדת בפרט!<br></br>
-                לפעילות מגיעים מדריך מוסמך ועוזר אחד לפחות, ניתן להתאים את מבנה הפעילות לכל רמה.
-              </p>
+              <h2>פעילות</h2>
+              <div className='text-center'>
+                <div className='font-bold'>שלב 1</div>
+                משחקונים ואתגרים חווייתיים שמטרתם חימום וכניסה לאווירת הפעילות.<br></br>
+                <div className='font-bold'>שלב 2</div>
+                תחנות שונות ומגוונות אותם מעבירים מדריכי הפעילות. סללומים וזריקה לסל, תחרות קליעות מעמדות שונות במגרש תוך מחשבה לאפשר לכל ילד/ה להנות בפעילות מיוחדת גם אם הם לא קשורים לענף הכדורסל.<br></br>
+                <div className='font-bold'>שלב 3</div>
+                משחק כדורסל להעצמת ילד יום ההולדת, הם מובילים הם הקפטנים והם מחלקי התשורות.<br></br>
+                <div className='font-bold'>שלב 4</div>
+                חלוקת פרסים ע"י ילד היום הולדת וטקס הדלקת הנרות יחד עם כל המשתתפים.
+              </div>
             </div>           
           </div>
         </div>
-        <GalleryConnector></GalleryConnector>
+        <RecommendationGalleryConnector></RecommendationGalleryConnector>
     </div>
   )
 }
